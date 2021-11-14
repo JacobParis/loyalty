61829e960cf0a601b9fd1e615c5bb2b4a1437a53
Jacob Paris <jacob@jacobpariseau.com>
2021-11-14 04:28:33 +0000

    Add auth and profile page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabaseClient'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Loyalty Cards</title>
-        <meta name="description" content="Loyalty cards" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [session, setSession] = useState(null)
 
-      <main>
-        <h1>
-          Loyalty Cards
-        </h1>
-      </main>
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
   )
 }
