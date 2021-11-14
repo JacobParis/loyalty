d5e43a8ac3e388ddc5875ad4bbfb8d3ace257a2a
Jacob Paris <jacob@jacobpariseau.com>
2021-11-14 03:23:11 +0000

    Initski commitski

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Loyalty Cards</title>
+        <meta name="description" content="Loyalty cards" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Loyalty Cards
+        </h1>
+      </main>
+    </div>
+  )
+}
